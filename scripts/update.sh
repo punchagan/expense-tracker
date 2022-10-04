46b6167604b30c51440abfc61b9ac4866bdb7cbf
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-10-04 12:19:53 +0530

    update: Handle the case when expenses.db isn't present correctly

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -xeuo pipefail
 
 HERE=$(dirname "$0")
-LAST_DATE=$(sqlite3 expenses.db 'SELECT MAX(date) FROM expenses;' | cut -d " " -f 1)
+LAST_DATE=$(sqlite3 expenses.db 'SELECT MAX(date) FROM expenses;' | cut -d " " -f 1) || true
 TODAY=$(date +%Y_%m_%d)
 
 # Download data
-pytest -sv "${HERE}/axis-scraper.py"  --browser=firefox --start-date "${LAST_DATE}" --workers=2 --reruns=3 --reruns-delay=20
+pytest -sv "${HERE}/axis-scraper.py" --browser=firefox --start-date "${LAST_DATE:-2022-01-01}" --workers=2 --reruns=3 --reruns-delay=20
 
 # Update new data in the database
 python "${HERE}/parse-data.py" "${HERE}/../downloaded_files/${AXIS_CUSTOMID}.csv" --catch-phrase 'Tran Date'
