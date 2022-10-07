1062df645a9679b44ca22c237f04f052ac43e271
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-10-07 11:27:02 +0530

    db: Add SQLAlchemy model and initial alembic migration

---
+++ app/model.py
@@ -0,0 +1,16 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+    id = sa.Column(sa.String(40), primary_key=True)
+    date = sa.Column(sa.DateTime(), nullable=False)
+    details = sa.Column(sa.Text(), nullable=False)
+    amount = sa.Column(sa.Float(asdecimal=True, decimal_return_scale=2), nullable=False)
+
+    def __repr__(self):
+        return f"Expense(date={self.date!r}, amount={self.amount!r} details={self.details!r})"
