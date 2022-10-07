f6ed1992cda6406aaa85c4a5ac4c4fdc3b909dd6
Tejaa Chintaluri <saitejaac@gmail.com>
2022-10-07 12:33:27 +0530

    Add .xls to .csv converter for sbi account statement

---
+++ scripts/sbi-read-csv.py
@@ -0,0 +1,57 @@
+import re
+import pandas as pd
+from pathlib import Path
+
+
+def read_file(from_filename, to_folderpath):
+    """
+    This function takes the filename of the sbi account statement
+    sbi-cc-statement.xls file and returns the path of csv file.
+    """
+    # Path variables
+    here = Path(__file__).parent.parent
+    # Get data from the downloaded folder - "./downloaded_files/sbi-cc-account.xls"
+    from_filename = here.joinpath(from_filename)
+
+    with open(from_filename) as f:
+        content = f.read()
+
+    content = content.replace(",", "").replace("\t", ",")
+    content = re.sub(" +", " ", content)
+
+    # Set the to_filename path for the output csv file
+    to_filename = str(from_filename).rsplit("/")[-1]
+    to_filename = to_filename.split(".")[0] + ".csv"
+    # Path for new filename
+    nfilename = here.joinpath(to_folderpath + "/" + to_filename)
+
+    with open(nfilename, "w") as f:
+        f.write(content)
+
+    df = pd.read_csv(nfilename, sep=",", header=None, names=[0, 1, 2, 3, 4, 5, 6, 7])
+    df.fillna(" ", inplace=True)
+    df = df[:-1]  # Remove "**This is a computer generate statement..."
+    df.to_csv(nfilename, header=False, index=False)
+    # Just return the dataframe after it is created - don't
+    # because use "sample/sbi-cc-statement.csv" for parsing
+    # os.remove(nfilename)
+    # return df
+    return nfilename
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--from-path", help="Path to the sbi account statement .xls file"
+    )
+    parser.add_argument(
+        "--to-folder-path",
+        help="Folder path for the to be sbi account statement output .csv file",
+    )
+
+    args = parser.parse_args()
+
+    csv_path = read_file(str(args.from_path), str(args.to_folder_path).rstrip("/"))
+    print("Successfully converted the sbi account statement file to csv at:", csv_path)
