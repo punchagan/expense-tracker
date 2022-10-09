7a4d84db744ce8f248f711d9f883497a80528c78
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-10-09 13:02:45 +0530

    sbi-read-csv: Clean-up function to generate clean CSV

--- scripts/sbi-read-csv.py
+++ scripts/sbi-read-csv.py
@@ -10,21 +10,15 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 from app.util import extract_csv
 
 
-def read_file(from_filename, to_folderpath):
-    """
-    This function takes the filename of the sbi account statement
-    sbi-cc-statement.xls file and returns the path of csv file.
-    """
-    # Path variables
-    here = Path(__file__).parent.parent
-    from_filename = here.joinpath(from_filename)
-    to_filename = from_filename.with_suffix(".csv").name
-    nfilename = Path(to_folderpath).absolute().joinpath(to_filename)
+def extract_clean_csv(from_filename, to_folderpath):
+    """Read a dirty TSV and dump a clean CSV."""
+    to_filename = Path(from_filename).with_suffix(".csv").name
+    to_path = Path(to_folderpath).absolute().joinpath(to_filename)
     text = extract_csv(from_filename, catch_phrase="Txn Date")
     df = pd.read_csv(text, sep="\t")
     df.columns = [c.strip() for c in df.columns]
-    df.to_csv(nfilename, index=False)
-    return nfilename
+    df.to_csv(to_path, index=False)
+    return to_path
 
 
 if __name__ == "__main__":
@@ -40,7 +34,5 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    csv_path = read_file(args.path, args.to_folder)
-    print(
-        f"Successfully converted the sbi account statement file to csv at: {csv_path}"
-    )
+    csv_path = extract_clean_csv(args.path, args.to_folder)
+    print(f"Wrote SBI account statement to: {csv_path}")
