cb7eed5b812f0768baa44aefd77abf3cdf4980cc
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-10-12 19:37:50 +0530

    doc: Document user-defined categories in the README

--- sample-conf.py
+++ sample-conf.py
@@ -1,2 +1,105 @@
 # List of additional user defined categories
-EXTRA_CATEGORIES = []
+EXTRA_CATEGORIES = [
+    # Food & Dining
+    "Groceries",
+    "Alcohol",
+    "Takeaway",
+    "Eating Out",
+    # Gifts & Donations
+    "Gift",
+    "Charity",
+    # Bills & Utilities
+    "Electricity",
+    "Heating & Cooling",
+    "Water Bill",
+    "Garbage Collection",
+    "Cell Phone Bill",
+    "Cable TV",
+    "Home Internet",
+    "Home Phone",
+    # Travel
+    "Travel Expense",
+    "Hotel",
+    "Trip Activities",
+    # Entertainment
+    "Movies",
+    "Art Shows",
+    "Streaming Subscriptions",
+    # Education
+    "Magazines",
+    "Books",
+    "Newspaper",
+    "Tuition Fee",
+    "Stationery",
+    "Hobby Education",
+    # Medical Expenses
+    "Medication ",
+    "Doctor Visit",
+    "Health insurance",
+    "Supplements",
+    "Glasses/Contact Lenses",
+    # Fitness
+    "Gym",
+    "Sports",
+    # Shopping
+    "Clothes",
+    "Shoes",
+    "Accessories",
+    # Goods & Gadgets
+    "Smartphone",
+    "Laptop",
+    "Wearable",
+    "Camera",
+    "Headphones/Speakers",
+    "Household Gadgets",
+    "Software",
+    "Hobby Supplies",
+    "Sporting Goods",
+    # Personal Care
+    "Laundry",
+    "Spa & Massage",
+    "Toiletries",
+    "Cosmetics",
+    "Hair Care",
+    # Kids
+    "Kids Activities",
+    "Kids Allowance",
+    "Baby Supplies",
+    "Babysitter & Daycare",
+    "Child Support",
+    "Kids Toys",
+    # Auto & Transport
+    "Fuel",
+    "Parking",
+    "Service & Auto Parts",
+    "Auto Payment",
+    "Auto Insurance",
+    "Public Transit",
+    "Taxi & Autorickshaw",
+    # Housing Expenses
+    "House Mortgage",
+    "House Rent",
+    "Home Insurance",
+    "Property Tax",
+    "House Repairs & Maintenance",
+    "Private Security Fees",
+    "Cleaning Services",
+    # Household Goods
+    "Furniture",
+    "Home Decorations",
+    "Household Appliances",
+    # Income
+    "Salary",
+    "Bonus",
+    "Tax Refund",
+    "Dividends",
+    "Earned Interest",
+    "Cashed-out Investment",
+    "Capital Gain",
+    "Insurance Income",
+    "Rental Income",
+    # Investing & Saving
+    "Savings Account",
+    "Retirement",
+    "Investment",
+]
