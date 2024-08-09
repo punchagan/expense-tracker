e1c40be167d92f0827b41b6e8c447134865331c3
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-08-09 17:04:40 +0530

    gdrive-csv: Download the CSV file to data git repo

--- scripts/gdrive-csv.py
+++ scripts/gdrive-csv.py
@@ -8,16 +8,15 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 import requests
-from app.util import ROOT
+from app.lib.git_manager import GitManager
 
 
 def download_sheet():
-    downloads = ROOT.joinpath("downloaded_files")
-    os.makedirs(downloads, exist_ok=True)
+    git_manager = GitManager()
     sheet_id = os.environ["GSHEET_ID"]
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
     response = requests.get(url)
-    csv_path = downloads.joinpath(f"{sheet_id}.csv")
+    csv_path = git_manager.repo_path.joinpath(f"manual-entries-{sheet_id}.csv")
     with open(csv_path, "w") as f:
         f.write(response.text)
 
