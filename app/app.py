a686e34c6ebc31417fcc28e4c1f4dca5132569ed
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-09-16 18:45:00 +0530

    app: Remove customized styling CSS

--- app/app.py
+++ app/app.py
@@ -550,7 +550,6 @@ def main():
         },
     )
 
-    local_css("style.css")
     # Enable Streamlit theme for Altair
     # alt.themes.enable("streamlit")      # FIXME: 0.15.0/1 broke Streamlit theme support
 
