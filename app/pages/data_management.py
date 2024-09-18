7751c1c455465c50762c1a7ea0e4131f8e749ff1
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-09-18 12:37:47 +0530

    data_management: Allow committing changes from the UI

--- app/pages/data_management.py
+++ app/pages/data_management.py
@@ -6,6 +6,15 @@ from app.components.git_status import setup_repo, check_git_status
 from app.lib.git_manager import get_repo_path, GitManager
 
 
+@st.dialog("Commit Changes to the Data Repository")
+def commit_dialog(git_manager: GitManager):
+    message = st.text_input("Enter commit message", max_chars=80)
+    if st.button("Commit"):
+        git_manager.commit_changes(message)
+        st.session_state.commit = True
+        st.rerun()
+
+
 def main():
     st.title("Data Git Repository Management")
 
@@ -26,6 +35,14 @@ def main():
             with st.expander("Repository changes ...", expanded=len(changes) <= 50):
                 md = f"```git\n{'\n'.join(changes)}\n```"
                 st.markdown(md)
+            if st.button("Commit Changes"):
+                commit_dialog(git_manager)
+        elif st.session_state.get("commit", False):
+            st.success("Changes committed successfully!")
+            st.balloons()
+            del st.session_state.commit
+        else:
+            st.success("No changes to commit.")
 
         if st.button("View Dashboard"):
             st.switch_page("pages/dashboard.py")
