4f756bb8795a6a9dd5c20ac0d78eec5faba6d51a
Puneeth Chaganti <punchagan@muse-amuse.in>
2024-09-29 09:23:21 +0530

    scrapers: Explicitly import scrapers for subclass list

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -1,3 +1,8 @@
+from .axis import AxisCCStatement, AxisStatement
 from .base import Source
+from .cash import CashStatement
+from .sbi import SBIStatement
 
 ALL_SCRAPERS = {kls.name: kls for kls in Source.__subclasses__()}
+
+__all__ = ["ALL_SCRAPERS", "AxisCCStatement", "AxisStatement", "CashStatement", "SBIStatement"]
